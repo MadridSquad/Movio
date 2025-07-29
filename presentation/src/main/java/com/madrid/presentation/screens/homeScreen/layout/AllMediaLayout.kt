04b9d3234ef11d17de6c133fb88965cb002ad421
yousef_osama11 <oyousef709@gmail.com>
2025-07-29 16:30:20 +0300

    add home screen layout

---
+++ presentation/src/main/java/com/madrid/presentation/screens/homeScreen/layout/AllMediaLayout.kt
@@ -0,0 +1,9 @@
+package com.madrid.presentation.screens.homeScreen.layout
+
+import androidx.compose.runtime.Composable
+import com.madrid.presentation.screens.homeScreen.component.TrendingLayout
+
+@Composable
+fun AllMediaLayout(){
+    TrendingLayout()
+}
