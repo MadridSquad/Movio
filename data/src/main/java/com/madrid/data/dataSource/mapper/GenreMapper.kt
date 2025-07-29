84091e63f9d03def5bb25b07ef5d2061ac4bb13a
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-07-29 21:18:44 +0300

    feat: add genre entity

--- data/src/main/java/com/madrid/data/dataSource/mapper/GenreMapper.kt
+++ data/src/main/java/com/madrid/data/dataSource/mapper/GenreMapper.kt
@@ -1,2 +1,21 @@
 package com.madrid.data.dataSource.mapper
 
+import com.madrid.data.dataSource.local.table.MovieGenreTable
+import com.madrid.data.dataSource.local.table.SeriesGenreTable
+import com.madrid.data.dataSource.remote.dto.genre.MovieGenre
+
+fun MovieGenre.toMovieGenreTable(): MovieGenreTable {
+    return MovieGenreTable(
+        genreId = this.id ?: 0,
+        genreTitle = this.name ?: "Unknown",
+        searchCount = 0
+    )
+}
+
+fun MovieGenre.toSeriesGenreTable(): SeriesGenreTable {
+    return SeriesGenreTable(
+        genreId = this.id ?: 0,
+        genreTitle = this.name ?: "Unknown",
+        searchCount = 0
+    )
+}
