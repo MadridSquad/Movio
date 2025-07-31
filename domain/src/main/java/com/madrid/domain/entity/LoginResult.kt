beee27a0f8733229fcd4abfa59f17fae197019ad
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-07-31 16:17:38 +0300

    refactor: Clean up unused imports and rename composable
    Signed-off-by: Yasser Ahmed <yasserahmed.fci@gmail.com>

--- domain/src/main/java/com/madrid/domain/entity/LoginResult.kt
+++ domain/src/main/java/com/madrid/domain/entity/LoginResult.kt
@@ -1,7 +1,5 @@
 package com.madrid.domain.entity
 
-import com.madrid.domain.entity.User
-
 sealed class LoginResult {
     data class Success(val user: User) : LoginResult()
     data class Error(val exception: Exception) : LoginResult()
