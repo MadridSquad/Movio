ca0a94b4d568293f1b233b69547a3885234eff5b
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-08-03 01:36:53 +0300

    feat: empty search preview

--- designSystem/src/main/java/com/madrid/designSystem/component/EmptySearchLayout.kt
+++ designSystem/src/main/java/com/madrid/designSystem/component/EmptySearchLayout.kt
@@ -18,8 +18,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.madrid.designSystem.theme.Theme
+import com.madrid.designSystem.R
 
 
 @Composable
@@ -64,5 +66,16 @@ fun EmptySearchLayout(
     }
 }
 
+
+@Preview
+@Composable
+private fun EmptySearchLayoutPreview() {
+    EmptySearchLayout(
+        title = "No Results Found",
+        description = "Try searching for something else.",
+        image = R.drawable.empty,
+        modifier = Modifier.padding(16.dp)
+    )
+}
 
 
