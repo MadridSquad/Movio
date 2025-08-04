45b5cc89566a8cc14301fc445b0c80a2b461e73f
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-08-04 14:21:16 +0300

    feat: enhance LoginHeader with padding and add preview function
    Signed-off-by: Yasser Ahmed <yasserahmed.fci@gmail.com>

--- presentation/src/main/java/com/madrid/presentation/screens/loginScreen/component/LoginHeader.kt
+++ presentation/src/main/java/com/madrid/presentation/screens/loginScreen/component/LoginHeader.kt
@@ -4,9 +4,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
@@ -15,9 +15,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.madrid.designSystem.R
 import com.madrid.designSystem.component.MovioText
+import com.madrid.designSystem.theme.MovioTheme
 import com.madrid.designSystem.theme.Theme
 
 
@@ -31,12 +33,13 @@ fun LoginHeader() {
 
 
         LogoWithBackground()
-        Spacer(modifier = Modifier.height(12.dp))
         MovioText(
             text = stringResource(com.madrid.presentation.R.string.welcome_back),
-            textStyle = Theme.textStyle.headline.largeBold18,
+            textStyle = Theme.textStyle.display.mediumMedium20,
             color = Theme.color.surfaces.onSurface,
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 12.dp),
             textAlign = TextAlign.Center
         )
 
@@ -66,5 +69,13 @@ fun LogoWithBackground() {
             contentDescription = "Movio Logo",
             modifier = Modifier.size(28.dp)
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginHeaderPreview() {
+    MovioTheme {
+        LoginHeader()
     }
 }
