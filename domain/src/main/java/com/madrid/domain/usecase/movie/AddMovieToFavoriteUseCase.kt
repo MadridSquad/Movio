9a41db2390d266481d11d65b456fe107ddc5a421
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-08-07 18:02:42 +0300

    resolve merge conflicts

--- domain/src/main/java/com/madrid/domain/usecase/movie/AddMovieToFavoriteUseCase.kt
+++ domain/src/main/java/com/madrid/domain/usecase/movie/AddMovieToFavoriteUseCase.kt
@@ -1,17 +1,17 @@
 package com.madrid.domain.usecase.movie
 
+import com.madrid.domain.repository.AuthenticationRepository
 import com.madrid.domain.repository.MovieRepository
-import com.madrid.domain.repository.UserRepository
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 class AddMovieToFavoriteUseCase @Inject constructor(
     val movieRepository: MovieRepository,
-    val userRepository: UserRepository
+    val authenticationRepository: AuthenticationRepository
 ) {
-     operator fun invoke(mediaId: Int) =
+    operator fun invoke(mediaId: Int) =
         runBlocking {
-            val sessionId = userRepository.getSessionId()
+            val sessionId = authenticationRepository.getSessionId()
             movieRepository.addMovieToFavorite(
                 mediaId = mediaId,
                 sessionId = sessionId.toString()
--- domain/src/main/java/com/madrid/domain/usecase/series/AddSeriesToFavoriteUseCase.kt
+++ domain/src/main/java/com/madrid/domain/usecase/series/AddSeriesToFavoriteUseCase.kt
@@ -1,16 +1,16 @@
 package com.madrid.domain.usecase.series
 
+import com.madrid.domain.repository.AuthenticationRepository
 import com.madrid.domain.repository.MovieRepository
-import com.madrid.domain.repository.UserRepository
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class AddSeriesToFavoriteUseCase @Inject constructor(
     val movieRepository: MovieRepository,
-    val userRepository: UserRepository
+    val authenticationRepository: AuthenticationRepository
 ) {
     suspend operator fun invoke(mediaId: Int) {
-        val sessionId: String = userRepository.getSessionId().first()
+        val sessionId: String = authenticationRepository.getSessionId().first()
         movieRepository.addMovieToFavorite(
             mediaId = mediaId,
             sessionId = sessionId
