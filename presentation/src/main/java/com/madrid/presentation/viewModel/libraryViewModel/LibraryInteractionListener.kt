882b33da7c8c9e07140720885f7b1421187ee334
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-08-08 04:25:47 +0300

    refactor getWatchListItemsUseCase using session id create onItemWatchListClick

--- presentation/src/main/java/com/madrid/presentation/viewModel/libraryViewModel/LibraryInteractionListener.kt
+++ presentation/src/main/java/com/madrid/presentation/viewModel/libraryViewModel/LibraryInteractionListener.kt
@@ -1,7 +1,8 @@
 package com.madrid.presentation.viewModel.libraryViewModel
 
 interface LibraryInteractionListener {
-    fun onItemClick(itemId: Int)
+    fun onItemClick(itemId: String)
+    fun onItemWatchListClick(watchListItem : WatchListState)
     /* Click on item (watchlistItem,favoriteItem, historyItem)*/
 
     fun onViewAllClick(type: String)
