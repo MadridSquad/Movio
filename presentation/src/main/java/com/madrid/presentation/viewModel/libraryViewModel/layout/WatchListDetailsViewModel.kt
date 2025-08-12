2091a1fed6351586d4efb3c3dfc299569a24cabe
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-08-13 01:31:44 +0300

    feature: implement delete movie from list in ui and view model

--- presentation/src/main/java/com/madrid/presentation/viewModel/libraryViewModel/layout/WatchListDetailsViewModel.kt
+++ presentation/src/main/java/com/madrid/presentation/viewModel/libraryViewModel/layout/WatchListDetailsViewModel.kt
@@ -1,8 +1,8 @@
 package com.madrid.presentation.viewModel.libraryViewModel.layout
 
-import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.toRoute
+import com.madrid.domain.usecase.movie.RemoveMovieFromListUseCase
 import com.madrid.domain.usecase.watchList.GetWatchListItemsUseCase
 import com.madrid.presentation.navigation.Destinations
 import com.madrid.presentation.viewModel.base.BaseViewModel
@@ -12,11 +12,11 @@ import javax.inject.Inject
 
 @HiltViewModel
 class WatchListDetailsViewModel @Inject constructor(
-   private val getWatchListItemsUseCase: GetWatchListItemsUseCase,
+    private val getWatchListItemsUseCase: GetWatchListItemsUseCase,
+    private val removeMovieFromList: RemoveMovieFromListUseCase,
     saveStateHandle: SavedStateHandle
-) : BaseViewModel<WatchListDetailsState, WatchListDetailsEffect>(
-    WatchListDetailsState()
-) {
+) : BaseViewModel<WatchListDetailsState, WatchListDetailsEffect>(WatchListDetailsState()),
+    WatchListDetailsInteractionListener {
     val args = saveStateHandle.toRoute<Destinations.WatchListDetailsScreen>()
 
     init {
@@ -31,27 +31,51 @@ class WatchListDetailsViewModel @Inject constructor(
             )
         }
         tryToExecute(
-            function = {
-                getWatchListItemsUseCase(args.watchListId)
-            },
-            onSuccess = {watchItemList->
-                var media = watchItemList.movies.map{it.toMediaUiState()} +watchItemList.series.map{it.toMediaUiState()}
-                updateState {
-                  it.copy(
-                      isLoading = false,
-                      watchList =  media
-                  )
-                }
-            },
-            onError = {throwable->
-                updateState {
-                    it.copy(
-                        isLoading = false,
-                        errorMessage = throwable.message
-                    )
-                }
-            },
+            function = { getWatchListItemsUseCase(args.watchListId) },
+            onSuccess = { onGetWatchListDetailsSuccess(it) },
+            onError = ::onError
         )
     }
 
+    private fun onGetWatchListDetailsSuccess(items: GetWatchListItemsUseCase.WatchListItems) {
+        updateState {
+            it.copy(
+                isLoading = false,
+                watchList = items.movies.map { movie -> movie.toMediaUiState() }
+            )
+        }
+    }
+
+    override fun onNavigateBack() {
+        emitNewEffect(WatchListDetailsEffect.NavigateBack)
+    }
+
+    override fun onClickMovieItem(movieId: String) {
+        emitNewEffect(WatchListDetailsEffect.NavigateToMovieDetails(movieId))
+    }
+
+    override fun onDeleteMovie(movieId: String) {
+        tryToExecute(
+            function = { removeMovieFromList(movieId.toInt(), args.watchListId) },
+            onSuccess = { onDeleteMovieSuccess(movieId) },
+            onError = ::onError
+        )
+    }
+
+    private fun onDeleteMovieSuccess(movieId: String) {
+        updateState { currentState ->
+            currentState.copy(
+                watchList = currentState.watchList.filterNot { it.id == movieId }
+            )
+        }
+    }
+
+    private fun onError(throwable: Throwable) {
+        updateState {
+            it.copy(
+                isLoading = false,
+                errorMessage = throwable.message
+            )
+        }
+    }
 }
