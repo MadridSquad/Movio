8dc3008c5b8b143e7b68ca98c671d3cf8ceab22e
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-08-16 23:24:03 +0300

    feature: implement loading state in CategoriesLayout

--- presentation/src/main/java/com/madrid/presentation/screens/homeScreen/layout/CategoriesLayout.kt
+++ presentation/src/main/java/com/madrid/presentation/screens/homeScreen/layout/CategoriesLayout.kt
@@ -4,17 +4,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import com.madrid.designSystem.component.FilterBar
 import com.madrid.designSystem.theme.Theme
 import com.madrid.presentation.component.movioCards.MovioVerticalCard
+import com.madrid.presentation.component.shimmer.ShimmerCard
 import com.madrid.presentation.viewModel.homeViewModel.CategoryUiState
 import com.madrid.presentation.viewModel.homeViewModel.SortingType
 import com.madrid.presentation.viewModel.shared.MediaType
@@ -29,7 +35,8 @@ fun CategoriesLayout(
     onSortingTypeSelected: (SortingType) -> Unit,
     onCategorySelected: (CategoryUiState) -> Unit,
     mediaItems: LazyPagingItems<MediaUiState>,
-    onMediaItemClicked: (Int, MediaType) -> Unit
+    onMediaItemClicked: (Int, MediaType) -> Unit,
+    isLoading: Boolean
 ) {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(minSize = 102.dp),
@@ -39,10 +46,11 @@ fun CategoriesLayout(
             .background(Theme.color.surfaces.surface),
         contentPadding = PaddingValues(horizontal = 16.dp),
         horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         item(span = { GridItemSpan(maxLineSpan) }) {
             FilterBar(
-                modifier = Modifier.padding(top = 16.dp),
+                modifier = Modifier.padding(top = 4.dp),
                 contentHorizontalPadding = 0.dp,
                 items = categories.map { it.name },
                 selectedItem = selectedCategory.name,
@@ -56,7 +64,7 @@ fun CategoriesLayout(
 
         item(span = { GridItemSpan(maxLineSpan) }) {
             FilterBar(
-                modifier = Modifier.padding(top = 12.dp, bottom = 24.dp),
+                modifier = Modifier.padding(bottom = 12.dp),
                 contentHorizontalPadding = 0.dp,
                 items = SortingType.entries.map { it.value },
                 selectedItem = sortingType.value,
@@ -68,20 +76,31 @@ fun CategoriesLayout(
             )
         }
 
-        items(mediaItems.itemCount) { index ->
-            MovioVerticalCard(
-                modifier = Modifier.padding(vertical = 12.dp),
-                description = mediaItems[index]?.title ?: "",
-                movieImage = mediaItems[index]?.imageUrl ?: "",
-                rate = mediaItems[index]?.rating ?: "",
-                height = 180.dp,
-                onClick = {
-                    onMediaItemClicked(
-                        mediaItems[index]?.id?.toIntOrNull() ?: 0,
-                        mediaItems[index]?.mediaType ?: MediaType.MOVIE
-                    )
-                }
-            )
+        if (mediaItems.loadState.refresh is LoadState.Loading || isLoading) {
+            items(9) {
+                ShimmerCard(
+                    isLoading = true,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(8.dp))
+                        .width(150.dp)
+                        .height(180.dp)
+                )
+            }
+        } else {
+            items(mediaItems.itemCount) { index ->
+                MovioVerticalCard(
+                    description = mediaItems[index]?.title ?: "",
+                    movieImage = mediaItems[index]?.imageUrl ?: "",
+                    rate = mediaItems[index]?.rating ?: "",
+                    height = 180.dp,
+                    onClick = {
+                        onMediaItemClicked(
+                            mediaItems[index]?.id?.toIntOrNull() ?: 0,
+                            mediaItems[index]?.mediaType ?: MediaType.MOVIE
+                        )
+                    }
+                )
+            }
         }
     }
 }
