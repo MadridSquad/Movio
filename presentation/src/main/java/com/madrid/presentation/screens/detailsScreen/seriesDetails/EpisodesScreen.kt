a0a368d44bb9dee1f9b972962f16a40f66c5e3a4
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-08-18 00:23:52 +0300

    solve conflict

--- presentation/src/main/java/com/madrid/presentation/screens/detailsScreen/seriesDetails/EpisodesScreen.kt
+++ presentation/src/main/java/com/madrid/presentation/screens/detailsScreen/seriesDetails/EpisodesScreen.kt
@@ -20,6 +20,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import androidx.hilt.navigation.compose.hiltViewModel
