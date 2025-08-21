24c08582a130026e6220870936dd2c7a495a101e
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2025-08-21 06:07:05 +0300

    feat: Integrate GlowingCircle component in SeeAllTVShowsScreen for enhanced UI effects

--- presentation/src/main/java/com/madrid/presentation/screens/homeScreen/SeeAllTVShowsScreen.kt
+++ presentation/src/main/java/com/madrid/presentation/screens/homeScreen/SeeAllTVShowsScreen.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -38,8 +37,8 @@ import com.madrid.designSystem.component.EmptySearchLayout
 import com.madrid.designSystem.component.FilterBar
 import com.madrid.designSystem.component.LoadingSearchCard
 import com.madrid.designSystem.component.TopAppBar
-import com.madrid.designSystem.theme.Theme
 import com.madrid.presentation.R
+import com.madrid.presentation.component.GlowingCircle
 import com.madrid.presentation.component.movioCards.MovioVerticalCard
 import com.madrid.presentation.navigation.Destinations
 import com.madrid.presentation.navigation.LocalNavController
@@ -69,129 +68,139 @@ fun SeeAllTVShowsScreen(
         }
     }
 
-    Column(
+    Box(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-
-        TopAppBar(
-            uiState.title,
-            secondIcon = null,
-            thirdIcon = null,
-            onFirstIconClick = {
-                navController.popBackStack(
-                    Destinations.HomeScreen,
-                    inclusive = false
-                )
-            },
+        GlowingCircle(
             modifier = Modifier
-                .padding(horizontal = 16.dp)
-                .padding(bottom = 16.dp)
                 .statusBarsPadding()
+                .align(Alignment.TopEnd)
         )
-        val updatedItems: MutableList<String> = items.map { it.name }.toMutableList()
-        updatedItems.add(0, "All")
-        FilterBar(
-            items = updatedItems,
-            selectedItem = selectedItem,
-            onItemClick = { genre ->
-                selectedItem = genre
-                viewModel.onGenreSelect(
-                    if (selectedItem != "All") items.find { it.name == genre } else null
-                )
-            },
-            scrollable = true
-        )
-        Spacer(Modifier.height(24.dp))
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(minSize = 101.33.dp),
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .background(Theme.color.surfaces.surface)
-                .navigationBarsPadding(),
-            contentPadding = PaddingValues(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(12.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp)
+                .fillMaxSize()
         ) {
-            when {
-                listOfItem.itemCount == 0 && listOfItem.loadState.refresh is LoadState.Loading -> {
-                    items(9) {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(200.dp),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            LoadingSearchCard()
+
+            TopAppBar(
+                uiState.title,
+                secondIcon = null,
+                thirdIcon = null,
+                onFirstIconClick = {
+                    navController.popBackStack(
+                        Destinations.HomeScreen,
+                        inclusive = false
+                    )
+                },
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .padding(bottom = 16.dp)
+                    .statusBarsPadding()
+            )
+            val updatedItems: MutableList<String> = items.map { it.name }.toMutableList()
+            updatedItems.add(0, "All")
+            FilterBar(
+                items = updatedItems,
+                selectedItem = selectedItem,
+                onItemClick = { genre ->
+                    selectedItem = genre
+                    viewModel.onGenreSelect(
+                        if (selectedItem != "All") items.find { it.name == genre } else null
+                    )
+                },
+                scrollable = true
+            )
+            Spacer(Modifier.height(24.dp))
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(minSize = 101.33.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .navigationBarsPadding(),
+                contentPadding = PaddingValues(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                when {
+                    listOfItem.itemCount == 0 && listOfItem.loadState.refresh is LoadState.Loading -> {
+                        items(9) {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(200.dp),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                LoadingSearchCard()
+                            }
+                        }
+                    }
+
+                    listOfItem.itemCount > 0 -> {
+                        items(
+                            count = listOfItem.itemCount,
+                        ) { index ->
+                            BoxWithConstraints(
+                                modifier = Modifier
+                            ) {
+                                val cardWidth = maxWidth
+                                val cardHeight = cardWidth * (136f / 101.33f)
+                                val movie = listOfItem[index]
+                                MovioVerticalCard(
+                                    modifier = Modifier.width(cardWidth),
+                                    description = movie?.name ?: "no description",
+                                    movieImage = movie?.imageUrl
+                                        ?: "https://image.tmdb.org/t/p/w500/5xKGk6q5g7mVmg7k7U1RrLSHwz6.jpg",
+                                    rate = movie?.rate?.take(3) ?: "4.3",
+                                    imageHeight = cardHeight,
+                                    onClick = {
+                                        navController.navigate(
+                                            Destinations.SeriesDetailsScreen(
+                                                listOfItem[index]!!.id.toInt(),
+                                                seasonNumber = 1
+                                            )
+                                        )
+                                    }
+                                )
+                            }
                         }
                     }
                 }
 
-                listOfItem.itemCount > 0 -> {
-                    items(
-                        count = listOfItem.itemCount,
-                    ) { index ->
-                        BoxWithConstraints(
-                            modifier = Modifier
-                        ) {
-                            val cardWidth = maxWidth
-                            val cardHeight = cardWidth * (136f / 101.33f)
-                            val movie = listOfItem[index]
-                            MovioVerticalCard(
-                                modifier = Modifier.width(cardWidth),
-                                description = movie?.name ?: "no description",
-                                movieImage = movie?.imageUrl
-                                    ?: "https://image.tmdb.org/t/p/w500/5xKGk6q5g7mVmg7k7U1RrLSHwz6.jpg",
-                                rate = movie?.rate?.take(3) ?: "4.3",
-                                imageHeight = cardHeight,
-                                onClick = {
-                                    navController.navigate(
-                                        Destinations.SeriesDetailsScreen(
-                                            listOfItem[index]!!.id.toInt(),
-                                            seasonNumber = 1
-                                        )
-                                    )
-                                }
-                            )
-                        }
-                    }
-                }
             }
 
-        }
+            AnimatedVisibility(
+                visible = listOfItem.itemCount == 0 && listOfItem.loadState.refresh is LoadState.Error,
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                DialogWithButtonLayout(
+                    title = stringResource(R.string.empty_no_internet_title),
+                    description = stringResource(R.string.empty_no_internet_description),
+                    image = R.drawable.img_no_internet,
+                    buttonText = stringResource(R.string.try_again),
+                    onClick = { viewModel.onTryAgainClick() },
+                    imageSize = 170,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp, vertical = 32.dp),
+                )
+            }
 
-        AnimatedVisibility(
-            visible = listOfItem.itemCount == 0 && listOfItem.loadState.refresh is LoadState.Error,
-            enter = fadeIn(),
-            exit = fadeOut()
-        ) {
-            DialogWithButtonLayout(
-                title = stringResource(R.string.empty_no_internet_title),
-                description = stringResource(R.string.empty_no_internet_description),
-                image = R.drawable.img_no_internet,
-                buttonText = stringResource(R.string.try_again),
-                onClick = { viewModel.onTryAgainClick() },
-                imageSize = 170,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 16.dp, vertical = 32.dp),
-            )
-        }
-
-        AnimatedVisibility(
-            visible = listOfItem.itemCount == 0 && listOfItem.loadState.refresh is LoadState.NotLoading,
-            enter = fadeIn(),
-            exit = fadeOut()
-        ) {
-            EmptySearchLayout(
-                title = stringResource(R.string.nothing_here_yet),
-                description = stringResource(R.string.category_empty_description),
-                image = R.drawable.img_no_sesrch_found,
-                imageSize = 170,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 16.dp, vertical = 32.dp),
-            )
+            AnimatedVisibility(
+                visible = listOfItem.itemCount == 0 && listOfItem.loadState.refresh is LoadState.NotLoading,
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                EmptySearchLayout(
+                    title = stringResource(R.string.nothing_here_yet),
+                    description = stringResource(R.string.category_empty_description),
+                    image = R.drawable.img_no_sesrch_found,
+                    imageSize = 170,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp, vertical = 32.dp),
+                )
+            }
         }
     }
 }
